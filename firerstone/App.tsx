42c860fca3ba392af178398a0c76c10f2bb071b1
Bruno César da Silva <57082742+bcs018@users.noreply.github.com>
2023-04-20 17:22:21 -0300

    Estudo firbase

---
+++ firerstone/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import messaging from '@react-native-firebase/messaging';
+
+export default () => {
+  return (
+    <SafeAreaView>
+        <Text>Qualquer Coisa</Text>
+    </SafeAreaView>
+  );
+}
